95d3f67a8acea4c463a0219668a6cbaaf3cefbd4
jkuehner <jochen.kuehner@kardex.com>
2024-01-11 00:12:19 +0100

    disable BaseCustomWebComp Parser for the moment, no esm version of typescript

--- src/webview/DesignerHtmlParserAndWriterService.ts
+++ src/webview/DesignerHtmlParserAndWriterService.ts
@@ -1,32 +1,32 @@
 import { IDesignItem, IHtmlParserService, InstanceServiceContainer, ServiceContainer } from "@node-projects/web-component-designer";
-import { BaseCustomWebcomponentParserService } from "@node-projects/web-component-designer-htmlparserservice-base-custom-webcomponent";
+//import { BaseCustomWebcomponentParserService } from "@node-projects/web-component-designer-htmlparserservice-base-custom-webcomponent";
 import { NodeHtmlParserService } from "@node-projects/web-component-designer-htmlparserservice-nodehtmlparser";
 
 export class DesignerHtmlParserAndWriterService implements IHtmlParserService {
 
 	nodeHtmlParserService: NodeHtmlParserService;
-	baseCustomWebcomponentParserService: BaseCustomWebcomponentParserService;
+	//baseCustomWebcomponentParserService: BaseCustomWebcomponentParserService;
 
 	public filename: string = '';
 	public originalContent: string = '';
 
 	constructor(path: string) {
 		this.nodeHtmlParserService = new NodeHtmlParserService();
-		this.baseCustomWebcomponentParserService = new BaseCustomWebcomponentParserService(this.nodeHtmlParserService);
+		//this.baseCustomWebcomponentParserService = new BaseCustomWebcomponentParserService(this.nodeHtmlParserService);
 	}
 
 	parse(html: string, serviceContainer: ServiceContainer, instanceServiceContainer: InstanceServiceContainer, parseSnippet: boolean): Promise<IDesignItem[]> {
 		this.originalContent = html;
-		if (this.filename.endsWith('.ts')) {
+		/*if (this.filename.endsWith('.ts')) {
 			return this.baseCustomWebcomponentParserService.parse(html, serviceContainer, instanceServiceContainer, parseSnippet);
-		}
+		}*/
 		return this.nodeHtmlParserService.parse(html, serviceContainer, instanceServiceContainer, parseSnippet);
 	}
 
 	write(html: string, css: string): string {
-		if (this.filename.endsWith('.ts')) {
+		/*if (this.filename.endsWith('.ts')) {
 			return this.baseCustomWebcomponentParserService.writeBack(this.originalContent, html, css, false);
-		}
+		}*/
 		return html;
 	}
 }
