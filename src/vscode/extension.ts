31c6e0fd1d0841f9646fc194f84cef9bd26e47ae
jkuehner <jochen.kuehner@kardex.com>
2024-02-02 19:05:08 +0100

    unused code

--- src/vscode/extension.ts
+++ src/vscode/extension.ts
@@ -1,19 +1,8 @@
 import * as vscode from 'vscode';
 import { DesignerTextEditor } from './DesignerTextEditor.js';
-import { DesignerTreeView } from './DesignerTreeView.js';
 
 export function activate(context: vscode.ExtensionContext) {
-	let t = new DesignerTreeView;
-	vscode.window.createTreeView("designerTreeView", {
-		treeDataProvider: t,
-		dragAndDropController: t,
-		showCollapseAll: true,
-		canSelectMany: true
-	});
-
 	//context.subscriptions.push(
 	//	vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
-
-
 	context.subscriptions.push(DesignerTextEditor.register(context));
 }
